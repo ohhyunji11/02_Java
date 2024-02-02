45cb175f7a2a06291a743623b0eb9880edf083ba
ohhyunji <ohhyunji11@gmail.com>
2024-02-02 17:54:48 +0900

    자바수업

--- 02_operator/src/edu/kh/op/practice/OperatorPractice.java
+++ 02_operator/src/edu/kh/op/practice/OperatorPractice.java
@@ -61,63 +61,63 @@ public class OperatorPractice {
   Scanner sc = new Scanner(System.in);
   
   System.out.print("이름 : ");
-  String name = sc.nextInt();
+  String name = sc.next();
   
   System.out.print("학년 : ");
   int grade = sc.nextInt();
   
   System.out.print("반 : ");
-  int calssroom = sc.nextInt();
+  int classroom = sc.nextInt();
   
   System.out.print("번호 : ");
   int number = sc.nextInt();
   
   System.out.print("성별(남학생/여학생) :");
-  String gender = sc.nextInt();
+  String gender = sc.next();
   
   System.out.print("성적 : ");
-  double scor = sc.nextDouble();
+  double score = sc.nextDouble();
   
   System.out.printf("%d학년 %d반 %번 %s %s의 성적은 %.2f이다.\n", grade, classroom, number, name, gender, score);
   
 }
 
-public void practice3( ) {
-	/*국어, 영어, 수학에 대한 점수를 키보드를 이용해 정수로 입력 받고,
-	세 과목에 대한 합계(국어+영어+수학)와 평균(합계/3.0)을 구하세요.
-	세 과목의 점수가 각각 40점 이상이면서 평균이 60점 이상일때 합격
-	아니면 불합격을 출력해라
-	[실행화면]
-	국어 : 60
-	영어 : 80
-	수학 : 40
-	합계 : 180
-	평균 : 60.0
-	합격
-	**/
+  public void practice3( ) {
+		/*국어, 영어, 수학에 대한 점수를 키보드를 이용해 정수로 입력 받고,
+		세 과목에 대한 합계(국어+영어+수학)와 평균(합계/3.0)을 구하세요.
+		세 과목의 점수가 각각 40점 이상이면서 평균이 60점 이상일때 합격
+		아니면 불합격을 출력해라
+		[실행화면]
+		국어 : 60
+		영어 : 80
+		수학 : 40
+		합계 : 180
+		평균 : 60.0
+		합격
+		**/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("국어 : ");
+		int kor = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		int eng = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		int math = sc.nextInt();
+		
+		int sum = kor + eng + math; // 합계
+		double avg = sum / 3.0; // 평균
+		
+		System.out.println("합계 : " + sum);
+		System.out.printf("평균 : %.1f\n", avg);
+		
+		boolean result = (kor >= 40) && (eng >=40) && (math >= 40) && (avg >= 60);
+		
+		System.out.println(result ? "합격" : "불합격");
 	
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.print("국어 : ");
-	int kor = sc.nextInt();
-	
-	System.out.print("영어 : ");
-	int eng = sc.nextInt();
-	
-	System.out.print("수학 : ");
-	int math = sc.nextInt();
-	
-	int sum = kor + eng + math; // 합계
-	double avg = sum / 3.0; // 평균
-	
-	System.out.println("합계 : " + sum);
-	System.out.printf("평균 : %.1f\n", avg);
-	
-	boolean result = (kor >= 40) && (eng >=40) && (math >= 40) && (avg >= 60);
-	
-	System.out.println(result ? "합격" : "불합격");
-	
-	
+  }
 	
 	
 }
