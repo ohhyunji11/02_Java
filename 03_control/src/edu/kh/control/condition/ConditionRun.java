cfa971019d057e7b1d8434e886bd258854dc09ce
ohhyunji <ohhyunji11@gmail.com>
2024-02-05 17:51:19 +0900

    자바 조건문

---
+++ 03_control/src/edu/kh/control/condition/ConditionRun.java
@@ -0,0 +1,28 @@
+package edu.kh.control.condition;
+
+import edu.kh.control.loop.WhileExample;
+
+public class ConditionRun { // 실행용 클래스
+
+ public static void main(String[] args) {
+	
+	 ConditionExample condition = new ConditionExample();
+	 // condition.ex1();
+	 //condition.ex2(); 
+	 //condition.ex3();
+	 //condition.ex4();
+	 //condition.ex5();
+	 //condition.ex6();
+	 //condition.ex7();
+	 //condition.ex8();
+	 
+	 
+	 WhileExample whileEx = new WhileExample();
+	 //whileEx.ex1();
+	 
+	 
+	 
+}
+	
+
+}
